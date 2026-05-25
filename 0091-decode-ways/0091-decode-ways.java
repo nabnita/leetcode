9612dd98037b138f35598444ef09231e4bfba7a0
nabnita <nabnitasingh082005@gmail.com>
2026-05-25 13:50:28 +0530

    Time: 1 ms (82.96%), Space: 43.3 MB (35.17%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] dp = new int[n+2];
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--){
+            if(s.charAt(i)=='0'){
+                dp[i]=0;
+            }
+            else{
+                dp[i]=dp[i+1];
+                if(i+1<n && Integer.parseInt(s.substring(i,i+2))<=26){
+                    dp[i]+=dp[i+2];
+                }
+            }
+        }
+        return dp[0];
+    }
+}
