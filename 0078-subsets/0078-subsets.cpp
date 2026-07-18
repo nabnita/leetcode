cb163fa2fc2de684859b9570557125673a08d077
nabnita <nabnitasingh082005@gmail.com>
2026-07-18 14:14:46 +0530

    Time: 2 ms (37.89%), Space: 9.9 MB (60.27%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void helper(int i, vector<int>& nums, vector<int>& curr, vector<vector<int>>& ans) {
+        if (i >= nums.size()) {
+            ans.push_back(curr);
+            return;
+        }
+        curr.push_back(nums[i]);
+        helper(i + 1, nums, curr, ans);
+        curr.pop_back();
+        helper(i + 1, nums, curr, ans);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        helper(0, nums, curr, ans);
+        return ans;
+    }
+};
